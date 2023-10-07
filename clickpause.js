d9bab7cb2d49ffd7fa86dd5566b4d0a68f9ce78b
hoamaibinhdinh <97245951+hoamaibinhdinh@users.noreply.github.com>
2023-10-07 21:56:55 +0700

    Add files via upload

---
+++ clickpause.js
@@ -0,0 +1,162 @@
+
+// pause video when click
+ var myVideo = document.getElementById("Vid");
+myVideo.addEventListener('click', function(e){
+   e.preventDefault();
+   this[this.paused ? 'play' : 'pause']();
+});
+  var myVideo = document.getElementById("next");
+myVideo.addEventListener('click', function(e){
+   e.preventDefault();
+   this[this.paused ? 'play' : 'pause']();
+});
+    var myVideo = document.getElementById("next1");
+myVideo.addEventListener('click', function(e){
+   e.preventDefault();
+   this[this.paused ? 'play' : 'pause']();
+});
+      var myVideo = document.getElementById("next2");
+myVideo.addEventListener('click', function(e){
+   e.preventDefault();
+   this[this.paused ? 'play' : 'pause']();
+});
+        var myVideo = document.getElementById("next3");
+myVideo.addEventListener('click', function(e){
+   e.preventDefault();
+   this[this.paused ? 'play' : 'pause']();
+});
+        var myVideo = document.getElementById("next4");
+myVideo.addEventListener('click', function(e){
+   e.preventDefault();
+   this[this.paused ? 'play' : 'pause']();
+});
+        var myVideo = document.getElementById("next5");
+myVideo.addEventListener('click', function(e){
+   e.preventDefault();
+   this[this.paused ? 'play' : 'pause']();
+});
+        var myVideo = document.getElementById("next6");
+myVideo.addEventListener('click', function(e){
+   e.preventDefault();
+   this[this.paused ? 'play' : 'pause']();
+});
+        var myVideo = document.getElementById("next7");
+myVideo.addEventListener('click', function(e){
+   e.preventDefault();
+   this[this.paused ? 'play' : 'pause']();
+});
+        var myVideo = document.getElementById("next8");
+myVideo.addEventListener('click', function(e){
+   e.preventDefault();
+   this[this.paused ? 'play' : 'pause']();
+});
+        var myVideo = document.getElementById("next9");
+myVideo.addEventListener('click', function(e){
+   e.preventDefault();
+   this[this.paused ? 'play' : 'pause']();
+});
+        var myVideo = document.getElementById("next10");
+myVideo.addEventListener('click', function(e){
+   e.preventDefault();
+   this[this.paused ? 'play' : 'pause']();
+});
+        var myVideo = document.getElementById("next11");
+myVideo.addEventListener('click', function(e){
+   e.preventDefault();
+   this[this.paused ? 'play' : 'pause']();
+});
+        var myVideo = document.getElementById("next12");
+myVideo.addEventListener('click', function(e){
+   e.preventDefault();
+   this[this.paused ? 'play' : 'pause']();
+});
+        var myVideo = document.getElementById("next13");
+myVideo.addEventListener('click', function(e){
+   e.preventDefault();
+   this[this.paused ? 'play' : 'pause']();
+});
+        var myVideo = document.getElementById("next14");
+myVideo.addEventListener('click', function(e){
+   e.preventDefault();
+   this[this.paused ? 'play' : 'pause']();
+});
+        var myVideo = document.getElementById("next15");
+myVideo.addEventListener('click', function(e){
+   e.preventDefault();
+   this[this.paused ? 'play' : 'pause']();
+});
+        var myVideo = document.getElementById("next16");
+myVideo.addEventListener('click', function(e){
+   e.preventDefault();
+   this[this.paused ? 'play' : 'pause']();
+});
+        var myVideo = document.getElementById("next17");
+myVideo.addEventListener('click', function(e){
+   e.preventDefault();
+   this[this.paused ? 'play' : 'pause']();
+});
+        var myVideo = document.getElementById("next18");
+myVideo.addEventListener('click', function(e){
+   e.preventDefault();
+   this[this.paused ? 'play' : 'pause']();
+});
+        var myVideo = document.getElementById("next19");
+myVideo.addEventListener('click', function(e){
+   e.preventDefault();
+   this[this.paused ? 'play' : 'pause']();
+});
+        var myVideo = document.getElementById("next20");
+myVideo.addEventListener('click', function(e){
+   e.preventDefault();
+   this[this.paused ? 'play' : 'pause']();
+});
+        var myVideo = document.getElementById("next21");
+myVideo.addEventListener('click', function(e){
+   e.preventDefault();
+   this[this.paused ? 'play' : 'pause']();
+});
+        var myVideo = document.getElementById("next22");
+myVideo.addEventListener('click', function(e){
+   e.preventDefault();
+   this[this.paused ? 'play' : 'pause']();
+});
+        var myVideo = document.getElementById("next23");
+myVideo.addEventListener('click', function(e){
+   e.preventDefault();
+   this[this.paused ? 'play' : 'pause']();
+});
+        var myVideo = document.getElementById("next24");
+myVideo.addEventListener('click', function(e){
+   e.preventDefault();
+   this[this.paused ? 'play' : 'pause']();
+});
+        var myVideo = document.getElementById("next25");
+myVideo.addEventListener('click', function(e){
+   e.preventDefault();
+   this[this.paused ? 'play' : 'pause']();
+});
+        var myVideo = document.getElementById("next26");
+myVideo.addEventListener('click', function(e){
+   e.preventDefault();
+   this[this.paused ? 'play' : 'pause']();
+});
+        var myVideo = document.getElementById("next27");
+myVideo.addEventListener('click', function(e){
+   e.preventDefault();
+   this[this.paused ? 'play' : 'pause']();
+});
+        var myVideo = document.getElementById("next28");
+myVideo.addEventListener('click', function(e){
+   e.preventDefault();
+   this[this.paused ? 'play' : 'pause']();
+});
+        var myVideo = document.getElementById("next29");
+myVideo.addEventListener('click', function(e){
+   e.preventDefault();
+   this[this.paused ? 'play' : 'pause']();
+});
+        var myVideo = document.getElementById("next30");
+myVideo.addEventListener('click', function(e){
+   e.preventDefault();
+   this[this.paused ? 'play' : 'pause']();
+});
